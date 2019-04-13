641ba1af8b3ba5711a0467e83f1cf43a8840088b
Ryan Mullin <ryan.mullin@wsu.edu>
2019-04-12 19:41:28 -0700

    Setup flask framework

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    """Serves the startup page for the website"""
+    return app.send_static_file('TODO: PUT STARTUP FILE NAME HERE')
+
+
+# info on uploading files: https://www.tutorialspoint.com/flask/flask_file_uploading.htm
+@app.route('/upload', methods=['POST'])
+def upload_data():
+    f = request.files['file']
 
 
 if __name__ == '__main__':
