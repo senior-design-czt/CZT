f33778c26c2194a0ac6c0585d1060a8576c81127
lukebrossman <lukebrossman1@gmail.com>
2019-04-14 22:58:30 -0700

    fixed some typecasting errors

--- proto_ml_regression.py
+++ proto_ml_regression.py
@@ -40,7 +40,6 @@ def getData(file):
   data = np.loadtxt(fname = file, delimiter = ';')
   features = data[1:,:]
   classLabels = data[0,:]
-  print(features)
   features = features.transpose()
   classLabels = classLabels.transpose()
   return features.tolist(), classLabels.tolist()
@@ -48,22 +47,21 @@ def getData(file):
 def dataFileToArray(file):
   with open(file, 'rU') as f:  
     reader = csv.reader(f)
-    data = list(list(rec) for rec in csv.reader(f, delimiter=',')) #reads csv into a list of lists
+    data = list(list(rec) for rec in csv.reader(f, delimiter=';')) #reads csv into a list of lists
     for row in range(len(data)):
       for item in range(len(data[row])):
         if data[row][item] == '':
           data[row][item] = '0'
-  datarray = [i for i in data[1:]]
-  
+  data = [i for i in data[1:]]
   newdata = []
-  for row in datarray:
-    newdata.append(row[1:])
+  for row in data:
+    newdata.append([float(j) for j in row[1:]])
 
   global features, classLabels
-  returndata = np.array(newdata)
-  features = returndata[1:,:]
-  classLabels = returndata[0,:]
-  print(features)
+  data = np.array(newdata)
+  features = data[1:,:]
+  classLabels = data[1,:]
+
   features = features.transpose()
   classLabels = classLabels.transpose()
 
@@ -73,10 +71,8 @@ def main():
   file_name = sys.argv[1]
   dataFileToArray(file_name)
                   
-  print(len(features[0]))
   print(findOptimalComponents())
-  print(classLabels)
-  print("10 fold scores for linear regression\n", cvs(LR(fit_intercept = False), features, classLabels, cv = 10).tolist(),"\n __________________________________\n")
+  print("10 fold scores for linear regression\n", cvs(LR(fit_intercept = False), features, classLabels, cv = 3).tolist(),"\n __________________________________\n")
   reg = LR(fit_intercept = True).fit(features, classLabels)
   print(reg.coef_)
 
