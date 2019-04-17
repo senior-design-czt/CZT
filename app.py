02000b02f3f86e923655a773b1edc3f69260a5a8
Ryan Mullin <ryan.mullin@sdcad.sdc.wsu.edu>
2019-04-17 15:18:08 -0700

    converted monolithic index.html to flask templates

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import os
 
 app = Flask(__name__)
-
-UPLOAD_DIR = os.getcwd() + '/data'
-app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
+app.config['UPLOAD_FOLDER'] = UPLOAD_DIR = '~/czt'
 
 
 @app.route('/')
-def index():
+@app.route('/home')
+def home():
     """Serves the startup page for the website"""
-    return app.send_static_file('index.html')
+    return render_template('home.html')
+
+
+@app.route('/data')
+def data():
+    """Serves the data page for the website"""
+    return render_template('data.html')
+
+
+@app.route('/status')
+def status():
+    """Serves the status page for the website"""
+    return render_template('status.html')
+
+
+@app.route('/compare')
+def compare():
+    """Serves the compare page for the website"""
+    return render_template('compare.html')
+
+
+@app.route('/layout')
+def layout():
+    """Serves the layout page for the website"""
+    return render_template('layout.html')
 
 
 # info on uploading files: https://www.tutorialspoint.com/flask/flask_file_uploading.htm
 @app.route('/upload', methods=['POST'])
-def upload_data():
+def upload():
     f = request.files['file']
     f.save(os.path.join(UPLOAD_DIR, f.filename))
     return 'saved'
